90cf9c12fcd0a82b906b2058e783a66dd9020687
adenbagja <adenbagja45@gmail.com>
2019-05-20 15:53:55 +0700

    tambah diskusi

--- application/views/forum2/p_diskusi.php
+++ application/views/forum2/p_diskusi.php
@@ -12,9 +12,9 @@
                      <form action="<?php echo base_url(). 'data_master/addnew'; ?>" method="post">
                     <div class="form-group col-sm-10">
                         <label for="judul_diskusi_d" style="float:left">Judul Diskusi</label>
-                        <input type="text" class="form-control" placeholder="Masukan judul Diskusi" name="judul_diskusi" id="exampleFormControlInput1">
+                        <input type="text" class="form-control" placeholder="Masukan judul Diskusi" name="judul_konten" id="exampleFormControlInput1">
                         <label for="isi_diskusi_d" style="float:left">Isi Diskusi</label>
-                        <textarea placeholder="Isi Diskusi" name="isi_diskusi" id="exampleFormControlInput1"></textarea>
+                        <textarea class="form-control" placeholder="Isi Diskusi" name="isi_konten" id="exampleFormControlInput1"></textarea>
                     </div>
                     <div class="col-sm-12">
                         <button type="submit" class="btn btn-primary">Tambah baru</button>
