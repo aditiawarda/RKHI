88993351ab7b4a0a6561ab7f30aa7d4e29a9646e
Iqbal Gumilar <iqbalgmlar@gmail.com>
2019-05-22 08:11:11 +0700

    fix register interface

--- application/views/auth/registrasi_user.php
+++ application/views/auth/registrasi_user.php
@@ -13,7 +13,7 @@
             <a href="#"><b class="primary-color">Rumah Konsultasi Hukum Islam</a>
         </div><!-- /.login-logo -->
         <div class="login-box-body">
-            <p class="login-box-msg text-bold"> Registrasi User Baru</p>
+            <p class="login-box-msg text-bold"> Silahkan isi data-data dibawah ini untuk Registrasi</p>
             <?php echo $message; ?>
             <?php
             echo form_open('admin/auth/register');
@@ -21,43 +21,43 @@
             <div class="row form-group">
                 <div class="col-lg-6">
                     <?php echo form_error('first_name') ?>
-                    <div class="input-group">
+                    <div class="form-group has-feedback">
                         <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Nama Depan">
-                        <span class="glyphicon  glyphicon-user form-control-feedback"></span>
+
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <?php echo form_error('last_name') ?>
-                    <div class="input-group">
+                    <div class="form-group has-feedback">
                         <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Nama Belakang">
-                        <span class="glyphicon  glyphicon-user form-control-feedback"></span>
+
                     </div>
                 </div>
             </div>
             <?php echo form_error('username') ?>
             <div class="form-group has-feedback">
                 <input type="text" name="username" id="username" class="form-control" placeholder="Nama Pengguna" />
-                <span class="glyphicon  glyphicon-user form-control-feedback"></span>
+
             </div>
             <?php echo form_error('company') ?>
             <div class="form-group has-feedback">
                 <input type="text" name="company" id="company" class="form-control" placeholder="Nama Perusahaan" />
-                <span class="glyphicon glyphicon-phone form-control-feedback"></span>
+
             </div>
             <?php echo form_error('email') ?>
             <div class="form-group has-feedback">
                 <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" />
-                <span class="glyphicon  glyphicon-envelope form-control-feedback"></span>
+
             </div>
             <?php echo form_error('password') ?>
             <div class="form-group has-feedback">
                 <input type="password" name="password" id="password" class="form-control" placeholder="Password" />
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
             </div>
             <?php echo form_error('password_confirm') ?>
             <div class="form-group has-feedback">
                 <input type="password" name="password_confirm" id="password_confirm" class="form-control" placeholder="Ulangi Password" />
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
             </div>
             <div class="col-xs-8">
 
